7a46851066b70634aa1c3d4d71583da118416e1c
Emmanuel Mayor <emayor@live.com.ar>
2022-05-07 01:42:35 -0300

    Se agregaron consignas en los ejercicios

--- TP5/ej1_emayor.py
+++ TP5/ej1_emayor.py
@@ -1,3 +1,14 @@
+# Modificar el TAD ListaEnlazada visto en la teoría, de tal forma que:
+# 1. No tenga la longitud de la lista en la cabecera (quitar el atributo len)
+# 2. Tenga un atributo ult_nodo, que sea una referencia al último nodo de la lista. ¿Cuales son las ventajas y
+# desventajas de agregar este atributo?
+# 3. Tenga por lo menos los siguientes métodos: __str__ , __len__ , agregar(x), indice(x)
+# Recordá modificar los métodos que se ven afectados por la desaparición/agregado de los atributos len y ult_nodo
+
+# La ventaja de tener un puntero al útlimo elemento es que para devolver el mismo el orden de complejidad pasaría a ser
+# constante, distinto a tener que recorrer la lista hasta encontrar el primer elemento que apunte a None, cuyo orden de
+# complejidad es lineal
+
 from ListaEn_Ej1 import *
 
 miLista = ListaEnlazada()
--- TP5/ej4_emayor.py
+++ TP5/ej4_emayor.py
@@ -1,4 +1,22 @@
-print("Hello World")
+# Crear un programa que, usando el TAD definido en el Ejercicio 1, permita a un usuario administrar una lista de tareas pendientes. Dá la opción de elegir entre las operaciones: agregar tareas, borrar tareas o listar las tareas de la lista.
+# Considerá lo siguiente:
+# • Las tareas se agregan al final de la lista, y llevan un número de ítem de lista y una descripción. El número se genera automáticamente a partir del número del último elemento de la lista + 1.
+# • Se puede borrar una tarea de cualquier lugar de la lista, utilizando el número de ítem.
+# • La opción de listar muestra todas las tareas pendientes que están en la lista, en el orden en que se agregaron.
 
-for i in "la concha de tu madre":
-    print(i)
+from ListaEn_Ej1 import ListaEnlazada
+
+listaTareas = ListaEnlazada()
+desc = -1
+
+while desc != '0':
+    desc = input("Ingrese el nombre de la tarea a agregar. Ingrese 0 para terminar\n")
+    if desc != '0':
+        nuevaTarea = (desc,len(listaTareas)+1)
+        listaTareas.append(nuevaTarea)
+        print(f"Se ha creado la tarea \"{desc}\"")
+while desc != '0':
+    desc = input("Ingrese el nombre de la tarea a borrar. Ingrese 0 para terminar\n")
+    if desc != '0':
+        pos = listaTareas.indice()
+print(listaTareas)
