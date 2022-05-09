6e3367b11a701c0f293594971fed9dd82e324a88
Emmanuel Mayor <emayor@live.com.ar>
2022-05-09 14:33:04 -0300

    Se creó el archivo del ejercicio 6

---
+++ TP5/ej6_emayor.py
@@ -0,0 +1,6 @@
+# Ejercicio 6
+# Escribí una función para mezclar dos listas enlazadas. Las listas de entrada tienen sus elementos en orden
+# creciente, de menor a mayor. La lista de salida también debe quedar ordenada, pero de mayor a menor (orden
+# decreciente).
+# El algoritmo debería tomar tiempo lineal, según la longitud de la lista de salida, que es la suma de la longitud de
+# las dos listas.
