a9301830ecb438d44da7a3c727f61097eb505ee4
Emmanuel Mayor <emayor@live.com.ar>
2022-05-09 16:36:05 -0300

    Se crea copia para ej5, se mueve la funcion

--- TP5/ListaEn_Ej1.py
+++ TP5/ListaEn_Ej1.py
@@ -157,36 +157,3 @@ class ListaEnlazada:
             # self.ult_nodo al elemento agregado
         if nuevo.proximo() == None:
             self.ult_nodo = nuevo
-
-def intercambiar(i, lista):
-    """ Invierte la posicion del i-elemento de la lista con su siguiente (i va de 0 a hasta len-1)"""
-    # Fijamos un elemento contiguo a otro
-    ant = lista.prim
-    act = ant.proximo()
-    sig = act.proximo()
-
-    # Si el índice es 0 (el primer elemento de la lista), entonces no hace falta referenciar al anterior
-    # Simplemente se establece el próximo como el primer elemento y a éste último como el que le sigue
-
-    if i == 0:
-        lista.prim.setproximo(sig)
-        act.setproximo(lista.prim)
-        lista.prim = act
-
-    # Si el índice está entre 0 y len-1, entonces buscamos el item moviendo de lugar
-    # los tres punteros ant, act y sig
-
-    elif i > 0 and i < len(lista)-1:
-        for i in range(0, i-1):
-            ant = ant.proximo()
-            act = act.proximo()
-            sig = sig.proximo()
-
-        # Reasignamos los proximos de cada nodo
-        ant.setproximo(sig)
-        act.setproximo(sig.proximo())
-        sig.setproximo(act)
-    # En cualquier otro caso, el índice está fuera de rango
-    else:
-        raise IndexError("Item fuera de rango")
-
