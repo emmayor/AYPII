fce3ea86cc2490f973b091a9be1578ddea46bf5f
Emmanuel Mayor <emayor@live.com.ar>
2022-05-09 18:01:13 -0300

    Comienzo del ejercicio 6

--- TP5/ej6_emayor.py
+++ TP5/ej6_emayor.py
@@ -4,3 +4,17 @@
 # decreciente).
 # El algoritmo debería tomar tiempo lineal, según la longitud de la lista de salida, que es la suma de la longitud de
 # las dos listas.
+# EJ:
+# L1 = [1, 12, 45, 46]
+# L2 = [0, 20, 40, 60, 80, 100]
+# L3 = [100, 80, 60, 46, 45, 40, 20, 12, 1, 0]
+
+from ListaEn_Ej5 import ListaEnlazada
+
+listaImpares = ListaEnlazada()
+listaPares = ListaEnlazada()
+
+def intercalarDesc(lista1, lista2):
+
+
+
