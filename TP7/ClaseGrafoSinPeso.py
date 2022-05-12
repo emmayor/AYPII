ed1e9f1a1d5719974b5d852cf35b6a2d55a8d214
Emmanuel Mayor <emayor@live.com.ar>
2022-05-11 22:10:57 -0300

    Preparacion clase grafo

---
+++ TP7/ClaseGrafoSinPeso.py
@@ -0,0 +1,54 @@
+from ClaseNodo import Nodo
+
+class Grafo:
+    """ Crea un grafo. Si es_dirigido se establece en True, 
+    todos los nodos que se inserten tendran aristas simples por defecto y aristas dobles en caso contrario"""
+    def __init__(self, es_dirigido=False):
+        self.lista_vertices = {}
+        self.n_arcos = 0
+        self.es_dirigido = es_dirigido
+
+    def __str__(self):
+        return "G = ("+str(self.n_vertices())+" V, "+str(self.n_arcos)+" A)"
+
+    def n_vertices(self):
+        return len(self.lista_vertices)
+
+    def dirigido(self):
+        return self.es_dirigido
+
+    def vertices(self):
+        return self.lista_vertices
+
+    def n_arcos(self):
+        return self.n_arcos
+
+    def insertarArco(self,x,z):
+        """Se carga el arco. ATENCION: Deben existir los vertices previamente"""
+        nw = Nodo(z, self.lista_vertices[x])
+        self.lista_vertices[x] = nw
+        self.n_arcos += 1
+
+    def insertar(self, x, z, excepcion=False):
+        """ Agregar el vértice de origen. Si excepcion es True, 
+        entonces se hace arista doble si es_dirigido = True 
+         y arista simple si es_dirigido = False """
+        if x not in self.lista_vertices:
+            self.lista_vertices[x] = None
+        """ Agregar el destino (ojo, x sumideros)"""
+        if z not in self.lista_vertices:
+            self.lista_vertices[z] = None
+        self.insertarArco(x, z)
+        if (not self.dirigido() ^ excepcion) and z != x:
+            self.insertarArco(z, x)
+
+    def imprimir(self):
+        for vertice in self.lista_vertices:
+            print(vertice, end=' ')
+            arco = self.lista_vertices[vertice]
+            if arco is not None:
+                print("->", end=' ')
+                arco.imprimirLista()
+            print("")
+           
+            
---
+++ TP7/testgrafo.py
@@ -0,0 +1,10 @@
+from ClaseGrafoSinPeso import Grafo
+
+grafo1 = Grafo() 
+
+a,b,c,d,e = "a","b","c","d","e"
+
+grafo1.insertar(a, b, True)
+
+
+grafo1.imprimir()
