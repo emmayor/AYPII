3aa783576bfb35543e8f6f765e7d070e1fef9c6a
Emmanuel Mayor <emayor@live.com.ar>
2022-06-08 03:24:10 -0300

    Updated GrafoPlot2.py

--- TP7/tads/GrafoPlot2.py
+++ TP7/tads/GrafoPlot2.py
@@ -1,140 +1,156 @@
 import pyglet 
-from math import atan, sin, cos
+
+from math import atan, sin, cos, pi
+
+# Enable Antialiasing
+pyglet.gl.glEnable(pyglet.gl.GL_LINE_SMOOTH)
+pyglet.gl.glEnable(pyglet.gl.GL_POLYGON_SMOOTH)
       
-class GNodo():
-    def __init__(self, dato, n_batch, x, y, radio):
-        self.radio = radio
-        self.dato = dato
+class GNode():
+    def __init__(self, name, x, y, r, n_batch):
+        self.radius = r
+        self.n_name = name
         self.batch = n_batch
         self.background = pyglet.graphics.OrderedGroup(0)
         self.foreground = pyglet.graphics.OrderedGroup(1)
         self.mouse_over = False
-        self.forma = pyglet.shapes.Circle(x, y, self.radio, 
+        self.shape = pyglet.shapes.Circle(x, y, self.radius, 
                                         batch=self.batch, group=self.background)
-        self.etiq = pyglet.text.Label(text=dato, color=(0,0,0,255), 
-                                    font_size=14,x=self.forma.x, y=self.forma.y,
-                                    anchor_x="center", anchor_y="center",
-                                    batch=self.batch, group=self.foreground)
-                
+        self.label = pyglet.text.Label(text=self.n_name, color=(0,0,0,255), 
+                                       font_size=14,x=self.shape.x, y=self.shape.y,
+                                       anchor_x="center", anchor_y="center",
+                                       batch=self.batch, group=self.foreground)
+
     def change_pos(self, x, y):
-        self.forma.x = x
-        self.forma.y = y
-        self.etiq.x = self.forma.x
-        self.etiq.y = self.forma.y
+        self.shape.x = x
+        self.shape.y = y
+        self.label.x = self.shape.x
+        self.label.y = self.shape.y
+
+    def name(self):
+        return self.n_name
 
     def x(self):
-        return self.forma.x
+        return self.shape.x
     
     def y(self):
-        return self.forma.y
+        return self.shape.y
 
     def r(self):
-        return self.radio
+        return self.radius
         
     def check_mouse_over(self,x,y):
-        m_range_x = x in range(self.forma.x-self.radio,self.forma.x+self.radio)
-        m_range_y = y in range(self.forma.y-self.radio,self.forma.y+self.radio)
+        m_range_x = x in range(self.shape.x-self.radius,self.shape.x+self.radius)
+        m_range_y = y in range(self.shape.y-self.radius,self.shape.y+self.radius)
         return m_range_x and m_range_y
 
     def on_mouse_motion(self, x, y, dx, dy):
         self.mouse_over = self.check_mouse_over(x,y)
         if self.mouse_over:
-            self.forma.color = (255, 242, 189)
+            self.shape.color = (255, 242, 189)
         else:
-            self.forma.color = (255, 255, 255)
-
+            self.shape.color = (255, 255, 255)
 
     def on_mouse_drag(self, x, y, dx, dy, buttons, modifiers):
         if self.mouse_over:
             self.change_pos(x,y)
 
-class GLinea():
-    def __init__(self, l_batch, nodo1, nodo2, double=False):
-        self.n1 = nodo1
-        self.n2 = nodo2
+class GArc():
+    def __init__(self, node_1, node_2,l_batch, double=False):
+        self.n1 = node_1
+        self.n2 = node_2
         self.batch = l_batch
-        self.linea = pyglet.shapes.Line(x=nodo1.x(), y=nodo1.y(), 
-                                        x2=nodo2.x(), y2=nodo2.y(), 
-                                        color=(255,255,255), batch=self.batch)
+        self.arc = pyglet.shapes.Line(x=self.n1.x(), y=self.n1.y(), 
+                                       x2=self.n2.x(),y2=self.n2.y(), 
+                                        color=(255,255,255), batch=self.batch)     
 
-        # PENDIENTE DE LA FLECHA (se puede evitar esta división asquerosa???)
-        if (self.linea.y2-self.linea.y) != 0:
-            self.pendiente = (self.linea.x2-self.linea.x)/(self.linea.y2-self.linea.y)
-        else: 
-            self.pendiente = 0.01
+        self.tip = pyglet.shapes.Triangle(x=0, y=0, 
+                                        x2=0, y2=0, 
+                                        x3=0, y3=0,
+                                        color=(255,255,255), batch=self.batch)      
+        self.update_pos()
 
-        # ANGULO DE LA FLECHA
-        self.angulo_p1 = atan(self.pendiente)
+    def calculate_tip_pos(self):
+        # ARROW SLOPE
+        if (self.arc.y2-self.arc.y) != 0:
+            self.slope = (self.arc.x2-self.arc.x)/(self.arc.y2-self.arc.y)
+        # ARROW ANGLE
+        if self.n1.y() < self.n2.y():
+            angle_t1 = atan(self.slope)+pi
+        else:
+            angle_t1 = atan(self.slope)
+        angle_t2 = angle_t1+0.26
+        angle_t3 = angle_t1-0.26
+        # TIP 1 COORDS (THE ONE THAT POINT TO THE NODE)
+        x_tip1 = sin(angle_t1)*self.n2.r()+self.n2.x()
+        y_tip1 = cos(angle_t1)*self.n2.r()+self.n2.y()
+        x_tip2 = sin(angle_t2)*(self.n2.r()+15)+self.n2.x()
+        y_tip2 = cos(angle_t2)*(self.n2.r()+15)+self.n2.y()
+        x_tip3 = sin(angle_t3)*(self.n2.r()+15)+self.n2.x()
+        y_tip3 = cos(angle_t3)*(self.n2.r()+15)+self.n2.y()
+        return (x_tip1,y_tip1,x_tip2,y_tip2,x_tip3,y_tip3)
 
-        # ANGULO DE LAS PUNTAS EXTERNAS
-        self.angulo_p2 = atan(self.pendiente+0.03)
-        self.angulo_p3 = atan(self.pendiente-0.03)
-        
-        # COORDENADAS DE LA PUNTA 1 (LA QUE ESTÁ PEGADA AL NODO)
-        self.x_punta = sin(self.angulo_p1)*self.n2.r()+self.n2.x()
-        self.y_punta = cos(self.angulo_p1)*self.n2.r()+self.n2.y()
-
-        # COORDENADAS DE LAS OTRAS DOS PUNTAS
-        self.x2_punta = sin(self.angulo_p2)*self.n2.r()+self.n2.x()+10
-        self.y2_punta = cos(self.angulo_p2)*self.n2.r()+self.n2.y()+10
-        self.x3_punta = sin(self.angulo_p3)*self.n2.r()+self.n2.x()-10
-        self.y3_punta = cos(self.angulo_p3)*self.n2.r()+self.n2.y()+10
-
-        # FORMA                 
-        self.punta1 = pyglet.shapes.Triangle(x=self.x_punta, y=self.y_punta,
-                                            x2=self.x2_punta, y2=self.y2_punta,
-                                            x3=self.x3_punta, y3=self.y3_punta,
-                                            color=(255,0,0), batch=self.batch)
-    
+    def update_pos(self):
+        self.arc.position = (self.n1.x(),self.n1.y(),self.n2.x(),self.n2.y())
+        self.tip.position = self.calculate_tip_pos()
+            
     def on_mouse_drag(self, x, y, dx, dy, buttons, modifiers):
-        self.linea.position = (self.n1.x(),self.n1.y(),self.n2.x(),self.n2.y())
-       # PENDIENTE DE LA FLECHA (se puede evitar esta división asquerosa???)
-        if (self.linea.y2-self.linea.y) != 0:
-            self.pendiente = (self.linea.y2-self.linea.y)/(self.linea.x2-self.linea.x)
-        else: 
-            self.pendiente = 0.01
+        self.update_pos()
 
-        # ANGULO DE LA FLECHA
-        self.angulo_p1 = atan(self.pendiente)
-
-        # ANGULO DE LAS PUNTAS EXTERNAS
-        self.angulo_p2 = atan(self.pendiente+0.33)
-        self.angulo_p3 = atan(self.pendiente-0.33)
-        
-        # COORDENADAS DE LA PUNTA 1 (LA QUE ESTÁ PEGADA AL NODO)
-        self.x_punta = sin(self.angulo_p1)*self.n2.r()+self.n2.x()
-        self.y_punta = cos(self.angulo_p1)*self.n2.r()+self.n2.y()
-
-        # COORDENADAS DE LAS OTRAS DOS PUNTAS
-        self.x2_punta = sin(self.angulo_p2)*(self.n2.r()+10)+self.n2.x()+10
-        self.y2_punta = cos(self.angulo_p2)*(self.n2.r()+10)+self.n2.y()+10
-        self.x3_punta = sin(self.angulo_p3)*(self.n2.r()+10)+self.n2.x()-10
-        self.y3_punta = cos(self.angulo_p3)*(self.n2.r()+10)+self.n2.y()+10
-
-        self.punta1.position = (self.x_punta,self.y_punta,
-                                self.x2_punta, self.y2_punta,
-                                self.x3_punta, self.y3_punta)
-  
 class GrafoPlot(pyglet.window.Window):
-    def __init__(self, width, height):
-        super().__init__(width, height, "Grafo")
+    def __init__(self, width=800, height=600):
+        super().__init__(width=width, height=height, caption="Grafo")
         self.batch = pyglet.graphics.Batch()    
-        self.nodos = []
-        #self.nodos.append(GNodo("EQS", self.batch, x=500, y=400, radio=30))
-        self.nodos.append(GNodo("TRV", self.batch, x=200, y=500, radio=30))
-        self.nodos.append(GNodo("BSA", self.batch, x=100, y=300, radio=30))
-        self.lineas = []
-        self.lineas.append(GLinea(self.batch, self.nodos[0], self.nodos[1], double=True))
-        #self.lineas.append(GLinea(self.batch, self.nodos[1], self.nodos[2], double=True))
-        for n in self.nodos:
+        self.nodes = {}
+        self.arcs = []
+        self.grid = []
+        self.push_node_handlers
+        self.push_arc_handlers
+
+    def generateGrid(self, size):
+        padding = 30
+        grid_width =  self.width  - 2*padding
+        grid_height = self.height - 2*padding
+        grid_node_dist_x = grid_width  // size 
+        grid_node_dist_y = grid_height // size 
+
+        for posx in range(size):
+            for posy in range(size):
+                self.grid.append((padding+grid_node_dist_x*posx,
+                                  padding+grid_node_dist_y*posy))
+
+    def push_node_handlers(self, node):
+        super().push_handlers(node.on_mouse_drag)
+        super().push_handlers(node.on_mouse_motion)
+
+    def push_arc_handlers(self, arc):
+        super().push_handlers(arc.on_mouse_drag)
+
+    def addNode(self,label,x,y,r):
+        new_node = GNode(label, x, y, r, self.batch)
+        self.nodes[new_node.name()] = [new_node]
+        self.push_node_handlers(new_node)
+        return new_node
+
+    def addArc(self,node_1,node_2):
+        new_arc = GArc(self.nodes[node_1], self.nodes[node_2], self.batch)
+        self.nodes[node_1.name()].append(new_arc)
+        self.push_arc_handlers(new_arc)
+
+    def push_all_handlers(self):
+        for n in self.nodes:
             super().push_handlers(n.on_mouse_drag)
             super().push_handlers(n.on_mouse_motion)
-        for l in self.lineas:
+        for l in self.arrows:
             super().push_handlers(l.on_mouse_drag)
+
     def on_draw(self):
         self.clear()
         self.batch.draw()
 
 if __name__ == '__main__':
     gplot = GrafoPlot(800,600)
+    gplot.addNode("EQS",500,400,30)
+    gplot.addNode("TRV",200,600,30)
+    gplot.addArc("EQS", "TRV")
     pyglet.app.run()
